ee883cd4d0af16043652a9e957bb0916c6295ebd
Shane M. Pelletier <lotios611@gmail.com>
2014-01-28 13:37:30 -0400

    Initial commit

---
+++ bot.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'twitter'
+require 'dotenv'
+Dotenv.load
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+end
